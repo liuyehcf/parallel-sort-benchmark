f964fcbf2725fa148c0a4ac8b8a1d20c772c65f9
liuyehcf <1559500551@qq.com>
2022-12-09 23:13:07 +0800

    test.sh

---
+++ test.sh
@@ -0,0 +1,632 @@
+# uniform distribution 
+build/benchmark --mode=1 \
+    --min_dop=1 --max_dop=16 \
+    --data_size=100000000 --data_distribution=1 \
+    --iteration=10
+
+build/benchmark --mode=2 \
+    --min_dop=1 --max_dop=16 \
+    --data_size=100000000 --data_distribution=1 \
+    --iteration=10
+
+build/benchmark --mode=3 \
+    --min_dop=1 --max_dop=16 \
+    --data_size=100000000 --data_distribution=1 \
+    --iteration=10
+
+build/benchmark --mode=4 \
+    --min_dop=1 --max_dop=16 \
+    --data_size=100000000 --data_distribution=1 \
+    --iteration=10
+
+build/benchmark --mode=5 \
+    --min_dop=1 --max_dop=16 \
+    --block_size=1000 \
+    --data_size=100000000 --data_distribution=1 \
+    --iteration=10
+
+build/benchmark --mode=5 \
+    --min_dop=1 --max_dop=16 \
+    --block_size=10000 \
+    --data_size=100000000 --data_distribution=1 \
+    --iteration=10
+
+build/benchmark --mode=5 \
+    --min_dop=1 --max_dop=16 \
+    --block_size=100000 \
+    --data_size=100000000 --data_distribution=1 \
+    --iteration=10
+
+build/benchmark --mode=5 \
+    --min_dop=1 --max_dop=16 \
+    --block_size=1000000 \
+    --data_size=100000000 --data_distribution=1 \
+    --iteration=10
+
+# normal distribution 
+build/benchmark --mode=1 \
+    --min_dop=1 --max_dop=16 \
+    --data_size=100000000 --data_distribution=2 \
+    --iteration=10
+
+build/benchmark --mode=2 \
+    --min_dop=1 --max_dop=16 \
+    --data_size=100000000 --data_distribution=2 \
+    --iteration=10
+
+build/benchmark --mode=3 \
+    --min_dop=1 --max_dop=16 \
+    --data_size=100000000 --data_distribution=2 \
+    --iteration=10
+
+build/benchmark --mode=4 \
+    --min_dop=1 --max_dop=16 \
+    --data_size=100000000 --data_distribution=2 \
+    --iteration=10
+
+build/benchmark --mode=5 \
+    --min_dop=1 --max_dop=16 \
+    --block_size=1000 \
+    --data_size=100000000 --data_distribution=2 \
+    --iteration=10
+
+build/benchmark --mode=5 \
+    --min_dop=1 --max_dop=16 \
+    --block_size=10000 \
+    --data_size=100000000 --data_distribution=2 \
+    --iteration=10
+
+build/benchmark --mode=5 \
+    --min_dop=1 --max_dop=16 \
+    --block_size=100000 \
+    --data_size=100000000 --data_distribution=2 \
+    --iteration=10
+
+build/benchmark --mode=5 \
+    --min_dop=1 --max_dop=16 \
+    --block_size=1000000 \
+    --data_size=100000000 --data_distribution=2 \
+    --iteration=10
+
+# skew distribution 
+build/benchmark --mode=1 \
+    --min_dop=1 --max_dop=16 \
+    --data_size=100000000 --data_distribution=3 \
+    --iteration=10
+
+build/benchmark --mode=2 \
+    --min_dop=1 --max_dop=16 \
+    --data_size=100000000 --data_distribution=3 \
+    --iteration=10
+
+build/benchmark --mode=3 \
+    --min_dop=1 --max_dop=16 \
+    --data_size=100000000 --data_distribution=3 \
+    --iteration=10
+
+build/benchmark --mode=4 \
+    --min_dop=1 --max_dop=16 \
+    --data_size=100000000 --data_distribution=3 \
+    --iteration=10
+
+build/benchmark --mode=5 \
+    --min_dop=1 --max_dop=16 \
+    --block_size=1000 \
+    --data_size=100000000 --data_distribution=3 \
+    --iteration=10
+
+build/benchmark --mode=5 \
+    --min_dop=1 --max_dop=16 \
+    --block_size=10000 \
+    --data_size=100000000 --data_distribution=3 \
+    --iteration=10
+
+build/benchmark --mode=5 \
+    --min_dop=1 --max_dop=16 \
+    --block_size=100000 \
+    --data_size=100000000 --data_distribution=3 \
+    --iteration=10
+
+build/benchmark --mode=5 \
+    --min_dop=1 --max_dop=16 \
+    --block_size=1000000 \
+    --data_size=100000000 --data_distribution=3 \
+    --iteration=10
+
+# uniform stream
+## k-merge
+build/benchmark --mode=6 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=1 \
+    --iteration=10 \
+    --chunk_size=4096
+
+build/benchmark --mode=6 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=1 \
+    --iteration=10 \
+    --chunk_size=8192
+
+build/benchmark --mode=6 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=1 \
+    --iteration=10 \
+    --chunk_size=16384
+
+build/benchmark --mode=6 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=1 \
+    --iteration=10 \
+    --chunk_size=32768
+
+build/benchmark --mode=6 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=1 \
+    --iteration=10 \
+    --chunk_size=65536
+
+build/benchmark --mode=6 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=1 \
+    --iteration=10 \
+    --chunk_size=131072
+
+build/benchmark --mode=6 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=1 \
+    --iteration=10 \
+    --chunk_size=262144
+
+build/benchmark --mode=6 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=1 \
+    --iteration=10 \
+    --chunk_size=524288
+
+build/benchmark --mode=6 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=1 \
+    --iteration=10 \
+    --chunk_size=1048576
+
+## parallel merge
+build/benchmark --mode=7 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=1 \
+    --iteration=10 \
+    --chunk_size=4096
+
+build/benchmark --mode=7 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=1 \
+    --iteration=10 \
+    --chunk_size=8192
+
+build/benchmark --mode=7 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=1 \
+    --iteration=10 \
+    --chunk_size=16384
+
+build/benchmark --mode=7 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=1 \
+    --iteration=10 \
+    --chunk_size=32768
+
+build/benchmark --mode=7 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=1 \
+    --iteration=10 \
+    --chunk_size=65536
+
+build/benchmark --mode=7 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=1 \
+    --iteration=10 \
+    --chunk_size=131072
+
+build/benchmark --mode=7 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=1 \
+    --iteration=10 \
+    --chunk_size=262144
+
+build/benchmark --mode=7 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=1 \
+    --iteration=10 \
+    --chunk_size=524288
+
+build/benchmark --mode=7 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=1 \
+    --iteration=10 \
+    --chunk_size=1048576
+
+## merge path merge
+build/benchmark --mode=8 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=1 \
+    --iteration=10 \
+    --chunk_size=4096
+
+build/benchmark --mode=8 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=1 \
+    --iteration=10 \
+    --chunk_size=8192
+
+build/benchmark --mode=8 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=1 \
+    --iteration=10 \
+    --chunk_size=16384
+
+build/benchmark --mode=8 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=1 \
+    --iteration=10 \
+    --chunk_size=32768
+
+build/benchmark --mode=8 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=1 \
+    --iteration=10 \
+    --chunk_size=65536
+
+build/benchmark --mode=8 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=1 \
+    --iteration=10 \
+    --chunk_size=131072
+
+build/benchmark --mode=8 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=1 \
+    --iteration=10 \
+    --chunk_size=262144
+
+build/benchmark --mode=8 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=1 \
+    --iteration=10 \
+    --chunk_size=524288
+
+build/benchmark --mode=8 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=1 \
+    --iteration=10 \
+    --chunk_size=1048576
+
+# normal stream
+## k-merge
+build/benchmark --mode=6 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=2 \
+    --iteration=10 \
+    --chunk_size=4096
+
+build/benchmark --mode=6 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=2 \
+    --iteration=10 \
+    --chunk_size=8192
+
+build/benchmark --mode=6 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=2 \
+    --iteration=10 \
+    --chunk_size=16384
+
+build/benchmark --mode=6 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=2 \
+    --iteration=10 \
+    --chunk_size=32768
+
+build/benchmark --mode=6 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=2 \
+    --iteration=10 \
+    --chunk_size=65536
+
+build/benchmark --mode=6 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=2 \
+    --iteration=10 \
+    --chunk_size=131072
+
+build/benchmark --mode=6 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=2 \
+    --iteration=10 \
+    --chunk_size=262144
+
+build/benchmark --mode=6 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=2 \
+    --iteration=10 \
+    --chunk_size=524288
+
+build/benchmark --mode=6 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=2 \
+    --iteration=10 \
+    --chunk_size=1048576
+
+## parallel merge
+build/benchmark --mode=7 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=2 \
+    --iteration=10 \
+    --chunk_size=4096
+
+build/benchmark --mode=7 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=2 \
+    --iteration=10 \
+    --chunk_size=8192
+
+build/benchmark --mode=7 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=2 \
+    --iteration=10 \
+    --chunk_size=16384
+
+build/benchmark --mode=7 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=2 \
+    --iteration=10 \
+    --chunk_size=32768
+
+build/benchmark --mode=7 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=2 \
+    --iteration=10 \
+    --chunk_size=65536
+
+build/benchmark --mode=7 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=2 \
+    --iteration=10 \
+    --chunk_size=131072
+
+build/benchmark --mode=7 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=2 \
+    --iteration=10 \
+    --chunk_size=262144
+
+build/benchmark --mode=7 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=2 \
+    --iteration=10 \
+    --chunk_size=524288
+
+build/benchmark --mode=7 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=2 \
+    --iteration=10 \
+    --chunk_size=1048576
+
+## merge path merge
+build/benchmark --mode=8 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=2 \
+    --iteration=10 \
+    --chunk_size=4096
+
+build/benchmark --mode=8 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=2 \
+    --iteration=10 \
+    --chunk_size=8192
+
+build/benchmark --mode=8 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=2 \
+    --iteration=10 \
+    --chunk_size=16384
+
+build/benchmark --mode=8 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=2 \
+    --iteration=10 \
+    --chunk_size=32768
+
+build/benchmark --mode=8 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=2 \
+    --iteration=10 \
+    --chunk_size=65536
+
+build/benchmark --mode=8 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=2 \
+    --iteration=10 \
+    --chunk_size=131072
+
+build/benchmark --mode=8 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=2 \
+    --iteration=10 \
+    --chunk_size=262144
+
+build/benchmark --mode=8 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=2 \
+    --iteration=10 \
+    --chunk_size=524288
+
+build/benchmark --mode=8 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=2 \
+    --iteration=10 \
+    --chunk_size=1048576
+
+# skew stream
+## k-merge
+build/benchmark --mode=6 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=3 \
+    --iteration=10 \
+    --chunk_size=4096
+
+build/benchmark --mode=6 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=3 \
+    --iteration=10 \
+    --chunk_size=8192
+
+build/benchmark --mode=6 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=3 \
+    --iteration=10 \
+    --chunk_size=16384
+
+build/benchmark --mode=6 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=3 \
+    --iteration=10 \
+    --chunk_size=32768
+
+build/benchmark --mode=6 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=3 \
+    --iteration=10 \
+    --chunk_size=65536
+
+build/benchmark --mode=6 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=3 \
+    --iteration=10 \
+    --chunk_size=131072
+
+build/benchmark --mode=6 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=3 \
+    --iteration=10 \
+    --chunk_size=262144
+
+build/benchmark --mode=6 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=3 \
+    --iteration=10 \
+    --chunk_size=524288
+
+build/benchmark --mode=6 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=3 \
+    --iteration=10 \
+    --chunk_size=1048576
+
+## parallel merge
+build/benchmark --mode=7 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=3 \
+    --iteration=10 \
+    --chunk_size=4096
+
+build/benchmark --mode=7 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=3 \
+    --iteration=10 \
+    --chunk_size=8192
+
+build/benchmark --mode=7 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=3 \
+    --iteration=10 \
+    --chunk_size=16384
+
+build/benchmark --mode=7 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=3 \
+    --iteration=10 \
+    --chunk_size=32768
+
+build/benchmark --mode=7 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=3 \
+    --iteration=10 \
+    --chunk_size=65536
+
+build/benchmark --mode=7 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=3 \
+    --iteration=10 \
+    --chunk_size=131072
+
+build/benchmark --mode=7 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=3 \
+    --iteration=10 \
+    --chunk_size=262144
+
+build/benchmark --mode=7 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=3 \
+    --iteration=10 \
+    --chunk_size=524288
+
+build/benchmark --mode=7 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=3 \
+    --iteration=10 \
+    --chunk_size=1048576
+
+## merge path merge
+build/benchmark --mode=8 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=3 \
+    --iteration=10 \
+    --chunk_size=4096
+
+build/benchmark --mode=8 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=3 \
+    --iteration=10 \
+    --chunk_size=8192
+
+build/benchmark --mode=8 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=3 \
+    --iteration=10 \
+    --chunk_size=16384
+
+build/benchmark --mode=8 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=3 \
+    --iteration=10 \
+    --chunk_size=32768
+
+build/benchmark --mode=8 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=3 \
+    --iteration=10 \
+    --chunk_size=65536
+
+build/benchmark --mode=8 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=3 \
+    --iteration=10 \
+    --chunk_size=131072
+
+build/benchmark --mode=8 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=3 \
+    --iteration=10 \
+    --chunk_size=262144
+
+build/benchmark --mode=8 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=3 \
+    --iteration=10 \
+    --chunk_size=524288
+
+build/benchmark --mode=8 \
+    --min_dop=2 --max_dop=16 \
+    --data_size=100000000 --data_distribution=3 \
+    --iteration=10 \
+    --chunk_size=1048576
